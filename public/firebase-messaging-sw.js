c8aec47c5578abd17ae8a09d8d8455fe51615406
Francesco <f.valli90@gmail.com>
2024-03-10 17:12:35 +0100

    try fix

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,86 +1,30 @@
-// importScripts(
-//   "https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js"
-// );
-// importScripts(
-//   "https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js"
-// );
-
-// // "Default" Firebase configuration (prevents errors)
-// const defaultConfig = {
-//   apiKey: true,
-//   projectId: true,
-//   messagingSenderId: true,
-//   appId: true,
-// };
-
-// // Initialize Firebase app
-// firebase.initializeApp(defaultConfig);
-// const messaging = firebase.messaging();
-
-// //Listens for background notifications
-// messaging.onBackgroundMessage((payload) => {
-//   console.log("Received background message: ", payload);
-
-//   //customise notification
-//   const notificationTitle = payload.notification.title;
-//   const notificationOptions = {
-//     body: payload.notification.body,
-//     icon: payload.notification.icon || "/icon.png",
-//   };
-
-//   self.registration.showNotification(notificationTitle, notificationOptions);
-// });
-
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
 importScripts(
-  "https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js"
-);
-importScripts(
-  "https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js"
+  "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
 );
 
-if (firebase.messaging.isSupported()) {
-  firebase.initializeApp({
-    apiKey: true,
-    projectId: true,
-    messagingSenderId: true,
-    appId: true,
-  });
+// "Default" Firebase configuration (prevents errors)
+const defaultConfig = {
+  apiKey: true,
+  projectId: true,
+  messagingSenderId: true,
+  appId: true,
+};
 
-  const messaging = firebase.messaging();
-  messaging.onBackgroundMessage((payload) => {
-    var notificationData = {};
-    if (payload.notification) {
-      notificationData = payload.notification;
-    } else {
-      notificationData = JSON.parse(payload.data.notification);
-    }
-    var notificationOptions = {
-      body: notificationData.body.replace(/<(.|\n)*?>/g, ""),
-      icon: notificationData.icon,
-    };
+// Initialize Firebase app
+firebase.initializeApp(defaultConfig);
+const messaging = firebase.messaging();
 
-    self.registration.showNotification(
-      notificationData.title,
-      notificationOptions
-    );
-  });
+//Listens for background notifications
+messaging.onBackgroundMessage((payload) => {
+  console.log("Received background message: ", payload);
 
-  self.addEventListener("notificationclick", function (event) {
-    console.dir(event);
-    event.notification.close();
+  //customise notification
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.icon || "/icon.png",
+  };
 
-    event.waitUntil(
-      clients
-        .matchAll({
-          type: "window",
-        })
-        .then(function (clientList) {
-          for (var i = 0; i < clientList.length; i++) {
-            var client = clientList[i];
-            if (client.url == "/" && "focus" in client) return client.focus();
-          }
-          if (clients.openWindow) return clients.openWindow("/");
-        })
-    );
-  });
-}
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
